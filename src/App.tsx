8ca1c77dbdf6f312e81ccca507e4f799b5412aa3
fkropfhamer <fabian.kropfhamer@sblum.de>
2020-11-24 15:36:27 +0100

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {apiEndpointUrl} from './constants'
 
 function App() {
+  function click() {
+    console.log('dfas')
+    fetch(apiEndpointUrl).then(r => r.json()).then(j => console.log(j));
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => click()} >Click me!</button>
     </div>
   );
 }
---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+export const apiUrl = process.env.REACT_APP_API_URL;
+export const apiEndpointUrl = `${apiUrl}/api/v1/`;
