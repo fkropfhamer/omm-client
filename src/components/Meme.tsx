c28b40a33a6ab789a7e4317593b7bc67add31cf0
fkropfhamer <fabian.kropfhamer@sblum.de>
2020-12-10 15:15:05 +0100

    added download button

--- src/components/Meme.tsx
+++ src/components/Meme.tsx
@@ -8,6 +8,23 @@ interface Props {
     meme: MemeObject
 }
 
+async function toDataURL(url: string) {
+    const response = await fetch(url)
+    const blob =  await response.blob();
+
+    return URL.createObjectURL(blob);
+
+}
+
+async function download(url: string, name: string) {
+    const a = document.createElement("a");
+    a.href = await toDataURL(url);
+    a.download = name;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
+
 export default function Meme(props: Props) {
     const {name, url, views} = props.meme;
 
@@ -15,5 +32,8 @@ export default function Meme(props: Props) {
         <h1>{name}</h1>
         <img src={url} alt={'meme with name "'+ name + '".'}></img>
         <h1>Views: {views}</h1>
+        <button onClick={()=> {
+            download(url, name);
+        }}>download</button>
     </div>
 }
