268a3f56e58e5c45a4f58d19a61c36a3bab7c24a
fkropfhamer <fabian.kropfhamer@sblum.de>
2020-12-10 14:56:51 +0100

    added views and links

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,7 +4,9 @@ export default function Home() {
     return (
         <div>
             <h1>Welcome</h1>
-            <Link to="/create-meme">create meme</Link>
+            <Link to="/create-meme">create meme</Link><br />
+            <Link to="/meme-slideshow">meme slideshow</Link><br />
+            <Link to="/meme-overview">meme overview</Link><br />
         </div>
     )
 }
--- src/components/Meme.tsx
+++ src/components/Meme.tsx
@@ -1,7 +1,7 @@
 export interface MemeObject {
     url: string,
     name: string,
-
+    views: number,
 }
 
 interface Props {
@@ -9,10 +9,11 @@ interface Props {
 }
 
 export default function Meme(props: Props) {
-    const {name, url} = props.meme;
+    const {name, url, views} = props.meme;
 
     return <div>
         <h1>{name}</h1>
         <img src={url} alt={'meme with name "'+ name + '".'}></img>
+        <h1>Views: {views}</h1>
     </div>
 }
