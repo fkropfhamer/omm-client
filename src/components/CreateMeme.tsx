c3bf59d1b3e76f4832df0ed016afc01ff257d46b
ShaohuaTong <shaohua.tong@stud.hs-emden-leer.de>
2021-01-17 23:12:32 +0100

    add Screenshot from provided URL

--- src/components/CreateMeme.tsx
+++ src/components/CreateMeme.tsx
@@ -3,10 +3,8 @@ import { RouteComponentProps } from "react-router-dom";
 import { apiEndpointUrl } from "../constants";
 
 interface State {
-
+    urlInput: string;
 }
-
-
 
 export default class CreateMeme extends React.Component<RouteComponentProps, State> {
     private fileInput: React.RefObject<any>;
@@ -15,6 +13,9 @@ export default class CreateMeme extends React.Component<RouteComponentProps, Sta
         super(props);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.fileInput = React.createRef();
+        this.state = {
+            urlInput: ""
+        }
     }
 
     private handleSubmit(event: React.FormEvent<HTMLFormElement>) {
@@ -35,6 +36,28 @@ export default class CreateMeme extends React.Component<RouteComponentProps, Sta
         .catch(err => console.log(err));
     }
 
+    private handleUrlSubmit= (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        const data = new FormData();
+        data.append("url", this.state.urlInput)
+
+        fetch(apiEndpointUrl + 'template', {
+            method: "POST",
+            body: data
+        })
+        .then(res => res.json()) 
+        .then(json => {
+            console.log(json);
+            this.props.history.push('/edit-meme/' + json.data.id)
+        })
+        .catch(err => console.log(err));
+    }
+
+    changeInputUrl = (event: React.FormEvent<HTMLInputElement>) => {
+        this.setState({urlInput: event.currentTarget.value});
+    }
+
     render() {
         return (
             <div>
@@ -42,6 +65,10 @@ export default class CreateMeme extends React.Component<RouteComponentProps, Sta
                 <form onSubmit={this.handleSubmit}>
                     <input type="file" ref={this.fileInput}></input>
                     <button type="submit">Submit</button>
+                </form>
+                <form onSubmit={this.handleUrlSubmit}>
+                    <input type="text" value={this.state.urlInput} onChange={this.changeInputUrl}></input>
+                    <button type="submit">Screenshot</button>
                 </form>
             </div>
         );
